8ab2539f060d507e15567487080521392beb2590
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-14 13:33:22 -0700

    Add class file

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: site_workflow
+# =======================
+#
+# Modules to hold files used in the workflow
+# There is no code in this module
+#
+# This is just a dummy class file for module completeness
+#
+class site_workflow: {
+}
